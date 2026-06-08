05fcbad0fe1fb50cf1438ed6469dfdfd492593f4
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-06-08 10:49:14 +0800

    lemonades: Add pixel experience flags up to 2021 when GMS is not included
    This can fix compatibility with certain Google apps for users using MicroG or alternative GMS packages.

--- avium.mk
+++ avium.mk
@@ -25,3 +25,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
      vendor/avium/prebuilt/common/etc/init/init.avium-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.avium-updater.rc
+
+# Pixel experience flags
+# For compatibility with certain google apps using MicroG.
+ifneq ($(WITH_GMS),true)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sysconfig/pixel_year_experience_default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/pixel_year_experience_default.xml
+endif
